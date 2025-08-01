3e77d7611f9bb11b769f67a09197ff83ea4b66cc
prakharsingh-74 <educationisfreedom.75938@gmail.com>
2025-08-01 11:41:30 +0530

    feat: implement session management with CRUD operations and routing

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const sessionRoutes = require("./routes/sessionRoutes");
 
 const app = express();
 
@@ -22,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //routes
 app.use("/api/auth", authRoutes);
-//app.use("/api/sessions", sessionRoutes);
+app.use("/api/sessions", sessionRoutes);
 //app.use("/api/questions", questionRoutes);
 
 //app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
