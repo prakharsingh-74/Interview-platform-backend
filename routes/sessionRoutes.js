c6c4988fc6ab45b5a4183d15fed2a218ee038e4b
prakharsingh-74 <educationisfreedom.75938@gmail.com>
2025-08-03 21:48:51 +0530

    fix: correct casing of authmiddleware import in sessionRoutes

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { createSession, getSessionById, getMySessions, deleteSession } = require("../controllers/sessionController");
-const { protect } = require("../middlewares/authMiddleware");
+const { protect } = require("../middlewares/authmiddleware");
 
 const router = express.Router();
 
